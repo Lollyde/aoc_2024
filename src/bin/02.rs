2aefff84d1b21c0fdd140c8762ee5450f50efbbe
Lollyde <github@lollyde.online>
2024-12-09 13:33:07 +0100

    day 2 wip

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,11 +1,41 @@
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
+    let input = parse_input(input);
+    //Some(input.filter(|r| {is_safe_report(r)}).collect().len())
     None
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     None
+}
+
+fn parse_input(input: &str) -> Vec<Vec<u32>> {
+    let mut out: Vec<Vec<u32>> = vec![];
+    input.lines().for_each(|r| {
+        out.push(r.split_ascii_whitespace().map(|l| {
+            l.parse().unwrap()
+        }).collect())
+    });
+    println!("{out:?}");
+    out
+}
+
+fn is_safe_report(input: &Vec<u32>) -> bool {
+    let mut iter = input.iter();
+    let mut a = iter.next().unwrap();
+    let mut b = iter.next().unwrap();
+    let inc = b > a;
+    loop {
+        b = match iter.next() {
+            Some(i) => i,
+            None => break
+        };
+        if !(a.abs_diff(*b) <= 4 && inc == (b > a)) {
+            return false;
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -15,7 +45,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 
     #[test]
